55b011a64dd250294cdc1cce3e99031931c0eb44
wejden7 <wejdenchneti7@gmail.com>
2024-05-02 11:18:45 +0100

    auth controller

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,69 @@
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    // HASH THE PASSWORD
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // CREATE NEW USER AND SAVE TO BD
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    console.log(newUser);
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.log("register controller error :", error);
+    res.status(500).json({ message: "Failed to create user!" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    //CHECK IF USER EXISTS
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) return res.status(401).json({ message: "Invalid Credentials!" });
+
+    //CHECK IF PASSWORD IS CORRECT
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid Credentials!" });
+
+    //GENERATE COOKIE TOKEN SEND TOKEN TO CLIENT
+
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: age,
+    });
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure : true,
+        maxAge: age,
+      })
+      .status(200)
+      .json({ message: "Login Successful" });
+  } catch (error) {
+    console.log("login controller error :", error);
+    res.status(500).json({ message: "Failed to login!" });
+  }
+};
+export const logout = (req, res) => {
+  res.clearCookie("token").status(200).json({ message: "Logout Successful" });
+};
