e1d6a948a79f6b711fd1750edb2261ef79593462
wejden7 <wejdenchneti7@gmail.com>
2024-05-02 13:22:18 +0100

    changed api

--- api/app.js
+++ api/app.js
@@ -1,12 +1,15 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
 
 const app = express();
-
+console.log(process.env.CLIENT_URL)
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
 app.use(express.json());
 app.use(cookieParser())
+
 
 app.use("/api/posts", postRouter);
 app.use("/api/auth", authRouter);
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -51,6 +51,7 @@ export const login = async (req, res) => {
       expiresIn: age,
     });
 
+    const {password:userPassword ,...userInfo} = user
     res
       .cookie("token", token, {
         httpOnly: true,
@@ -58,7 +59,7 @@ export const login = async (req, res) => {
         maxAge: age,
       })
       .status(200)
-      .json({ message: "Login Successful" });
+      .json(userInfo);
   } catch (error) {
     console.log("login controller error :", error);
     res.status(500).json({ message: "Failed to login!" });
